3602854370efff2f37792ace2c6101ebcbd2e0bd
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2018-12-10 09:33:12 -0500

    Step 1 - Raise ValueError for string values

--- prime.py
+++ prime.py
@@ -1,3 +1,6 @@
 """
 prime.py -- Write the application code here
 """
+def generate_prime_factors(value):
+    """Returns a list of primes."""
+    raise ValueError()
--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -2,3 +2,11 @@
 """
 The test module for Prime Factors
 """
+import pytest
+
+from prime import generate_prime_factors
+
+
+def test_string_values_are_rejected():
+    with pytest.raises(ValueError):
+        generate_prime_factors('banana')
